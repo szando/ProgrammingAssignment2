39b36517a272c3eef1988b2f18c320bf934daf2a
szando <sandor.boros@linux.com>
2014-10-26 13:10:21 +0100

    Hopefully git works from the command line at least

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The below functions work together to invert a matrix and cache the results,
+# so the next time the functions are invoked the inverse of the matrix will be retrieved 
+# from the cache, rather than recalculated.
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(solve) inv <<- solve
+  getinvert <- function() inv
+  list(set = set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvert()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinvert(inv)
+  inv
 }
